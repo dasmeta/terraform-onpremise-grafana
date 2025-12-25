00944afc763edaead25a498928c80ea34ca977a0
Vazgen Sargsyan <vazgen@dasmeta.com>
2025-12-25 14:58:09 +0400

    fix(DMVP-8526): Add initial setup and example dashboard configuration for Grafana

---
+++ tests/dashboard-blocks/0-setup.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.3"
+
+  required_providers {
+    grafana = {
+      source  = "grafana/grafana"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# you can start dev grafana server locally using `docker compose up -d` from `/tests` folder before running the test locally
+provider "grafana" {
+  url  = "https://grafana.example.com"
+  auth = "admin:admin"
+}
