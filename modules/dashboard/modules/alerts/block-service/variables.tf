f1c663a9ba3b2336f3701a5a193f2b5564d97b1f
Vazgen Sargsyan <vazgen@dasmeta.com>
2026-01-14 18:44:33 +0400

    fix(DMVP-9178): Remove default priority label in variable alerts

--- modules/dashboard/modules/alerts/block-service/variables.tf
+++ modules/dashboard/modules/alerts/block-service/variables.tf
@@ -36,13 +36,13 @@ variable "defaults" {
 variable "alerts" {
   type = object({
     replicas_no = optional(object({
-      enabled        = optional(bool, null)                 # whether to have alert if no any replica/pod available
-      pending_period = optional(string, "0s")               # define for how long to wait to trigger alert if condition satisfied(how long should satisfied state last to fire), if set `null` here it takes  defaults value
-      labels         = optional(any, { "priority" : "P1" }) # define alert labels to filter in notification policies, this extends with override the defaults labels. we set here P1 priority as if there are no any pods the service is down
-      exec_err_state = optional(string, null)               # define how to handle if query execution error, if set `null` here it takes  defaults value
-      no_data_state  = optional(string, null)               # define how to handle if no data for query, if set `null` here it takes  defaults value
-      group          = optional(string, null)               # grafana alert group name which used for grouping, if set `null` here it takes  defaults value
-      annotations    = optional(any, {})                    # define alert annotations to include in notifications
+      enabled        = optional(bool, null)   # whether to have alert if no any replica/pod available
+      pending_period = optional(string, "0s") # define for how long to wait to trigger alert if condition satisfied(how long should satisfied state last to fire), if set `null` here it takes  defaults value
+      labels         = optional(any, {})      # define alert labels to filter in notification policies, this extends with override the defaults labels. we set here P1 priority as if there are no any pods the service is down
+      exec_err_state = optional(string, null) # define how to handle if query execution error, if set `null` here it takes  defaults value
+      no_data_state  = optional(string, null) # define how to handle if no data for query, if set `null` here it takes  defaults value
+      group          = optional(string, null) # grafana alert group name which used for grouping, if set `null` here it takes  defaults value
+      annotations    = optional(any, {})      # define alert annotations to include in notifications
     }), {})
     replicas_min = optional(object({
       enabled        = optional(bool, null)   # whether to have alert on min replicas/pods, so that if there are no at least min count of pods/replicas it will trigger alert
