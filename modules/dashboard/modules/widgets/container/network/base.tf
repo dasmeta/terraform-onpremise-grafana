8de97c2a4b2b2f6eb0b060df5a219c46c1f89822
Adeline Eminian <Adeline@ip-192-168-88-22.eu-central-1.compute.internal>
2026-09-15 12:30:37 +0400

    fix: match rollout pod names in dashboards

--- modules/dashboard/modules/widgets/container/network/base.tf
+++ modules/dashboard/modules/widgets/container/network/base.tf
@@ -11,10 +11,10 @@ module "base" {
   unit        = "bytes"
 
   metrics = concat([
-    { label = "Received", expression = "sum(rate(container_network_receive_bytes_total{namespace=\"${var.namespace}\", pod=~\"^${var.container}-[^-]+-[^-]+$\"}[${var.period}]))" },
-    { label = "Sent", expression = "- sum(rate(container_network_transmit_bytes_total{namespace=\"${var.namespace}\", pod=~\"^${var.container}-[^-]+-[^-]+$\"}[${var.period}]))" },
-    { label = "Received ({{pod}})", color = "3ECE76", expression = "sum(rate(container_network_receive_bytes_total{namespace=\"${var.namespace}\", pod=~\"^${var.container}-[^-]+-[^-]+$\"}[${var.period}])) by (pod)" },
-    { label = "Sent ({{pod}})", color = "FF0F3C", expression = "- sum(rate(container_network_transmit_bytes_total{namespace=\"${var.namespace}\", pod=~\"^${var.container}-[^-]+-[^-]+$\"}[${var.period}])) by (pod)" },
+    { label = "Received", expression = "sum(rate(container_network_receive_bytes_total{namespace=\"${var.namespace}\", pod=~\"^${var.container}(-[^-]+)?-[^-]+-[^-]+$\"}[${var.period}]))" },
+    { label = "Sent", expression = "- sum(rate(container_network_transmit_bytes_total{namespace=\"${var.namespace}\", pod=~\"^${var.container}(-[^-]+)?-[^-]+-[^-]+$\"}[${var.period}]))" },
+    { label = "Received ({{pod}})", color = "3ECE76", expression = "sum(rate(container_network_receive_bytes_total{namespace=\"${var.namespace}\", pod=~\"^${var.container}(-[^-]+)?-[^-]+-[^-]+$\"}[${var.period}])) by (pod)" },
+    { label = "Sent ({{pod}})", color = "FF0F3C", expression = "- sum(rate(container_network_transmit_bytes_total{namespace=\"${var.namespace}\", pod=~\"^${var.container}(-[^-]+)?-[^-]+-[^-]+$\"}[${var.period}])) by (pod)" },
     ],
     var.host != null ? [{ label = "In(ingress)", expression = "sum(rate(nginx_ingress_controller_bytes_sent_bucket{host=\"${var.host}\"}[${var.period}]))" }] : [],
   )
